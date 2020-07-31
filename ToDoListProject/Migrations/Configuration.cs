26ac7a280ca2046c2f2450cec4719bdd83d8488b
ChrisRusev <kristianrusev@gmail.com>
2020-07-31 12:12:27 +0300

    ToDoList

---
+++ ToDoListProject/Migrations/Configuration.cs
@@ -0,0 +1,34 @@
+namespace ToDoListProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using ToDoListProject.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ToDoListProject.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ToDoListProject.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            AddUsers(context);
+        }
+        void AddUsers(ToDoListProject.Models.ApplicationDbContext context)
+        {
+            var user = new ApplicationUser { UserName = "user1@email.com" };
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            um.Create(user, "password");
+        }
+    }
+}
